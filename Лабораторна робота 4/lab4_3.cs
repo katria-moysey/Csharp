ec8af06a1731e8824a10c09fa9ca2db6634a6481
Katria <moisei.kateryna@student.uzhnu.edu.ua>
2026-03-18 08:40:16 +0200

    Лабораторна робота 4

--- Лабораторна робота 4/lab4_3.cs
+++ Лабораторна робота 4/lab4_3.cs
@@ -5,7 +5,7 @@ using System;
 namespace Aplication5
 {
     class Program{
-        // Рекурсивний (повільніший)
+        // Рекурсивний 
             static double Num(int x)
             {
                 if (x == 0) return 2;
@@ -13,32 +13,12 @@ namespace Aplication5
 
                 return (Num(x - 1) * (1 + Num(x - 2)) + Num(x - 3)) / Num(x - 4);
             }
-        // Ітеративний (швидкіший)
-            static double Nums(int n)
-        {
-            if (n == 0) return 2;
-            if (n >= 1 && n <= 3) return 7;
-            double[] x = new double[n + 1];
-            x[0] = 2;
-            x[1] = 7;
-            x[2] = 7;
-            x[3] = 7;
-
-            for (int i = 4; i <= n; i++)
-            {
-                x[i] = (x[i - 1] * (1 + x[i - 2]) + x[i - 3]) / x[i - 4];
-            }
-
-            return x[n];
-        }
-
         
         static void Main()
         {
             Console.WriteLine("Уведіть число: ");
             int n=int.Parse(Console.ReadLine());
             double number2=Num(n);
-            double number1=Nums(n);
             Console.WriteLine($"Число {number1}");
             Console.WriteLine($"Число {number2}");
         }
